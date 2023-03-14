2b764d132e3a5e77a6cce223a40d3bb1df105805
youngjae459 <youngjae459@gmail.com>
2023-03-14 13:57:39 +0900

    다양한 리턴타입의 메소드확인

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -1,19 +1,29 @@
 package com.ll.basic1;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 // @Controller 의 의미
 // 개발자가 스프링부트에게 말한다.
 // 아래 있는 HomeController 는 컨트롤러이다.
 public class HomeController {
-    private int count;
+    private int id;
+    private List<Person> personList;
 
     public HomeController() {
-        this.count = 0;
+        this.id = 0;
+        personList = new ArrayList<>();
     }
 
     // @GetMapping("/home/main") 의 의미
@@ -48,7 +58,7 @@ public class HomeController {
     // 아래 메서드를 실행한 후 그 리턴값을 응답으로 삼아줘
     @ResponseBody
     public int showIncrese() {
-        return count++;
+        return id++;
     }
 
     @GetMapping("/home/plus")
@@ -59,4 +69,200 @@ public class HomeController {
     public int showPlus(@RequestParam(defaultValue = "1") int a, int b) {
         return a + b;
     }
+
+    @GetMapping("/home/addPerson")
+    @ResponseBody
+    public String showAddPerson(String name, int age) {
+        personList.add(new Person(id++, name, age));
+        return id + "번 사람이 추가되었습니다.";
+    }
+
+    @GetMapping("/home/people")
+    @ResponseBody
+    public List<Person> showPeople() {
+        return personList;
+    }
+
+    // 다양한 리턴 타입으로 응답해보기
+    @GetMapping("/home/returnBoolean")
+    @ResponseBody
+    public boolean showReturnBoolean() {
+        return true;
+    }
+
+    @GetMapping("/home/returnDouble")
+    @ResponseBody
+    public double showReturnDouble() {
+        return Math.PI;
+    }
+
+    @GetMapping("/home/returnIntArray")
+    @ResponseBody
+    public int[] showReturnIntArray() {
+        int[] arr = new int[]{10, 20, 30};
+
+        return arr;
+    }
+
+    @GetMapping("/home/returnIntList")
+    @ResponseBody
+    public List<Integer> showReturnIntList() {
+        List<Integer> list = new ArrayList<>() {{
+            add(10);
+            add(20);
+            add(30);
+        }};
+
+        return list;
+    }
+
+    @GetMapping("/home/returnMap")
+    @ResponseBody
+    public Map<String, Object> showReturnMap() {
+        Map<String, Object> map = new LinkedHashMap<>() {{
+            put("id", 1);
+            put("speed", 100);
+            put("name", "카니발");
+            put("relatedIds", new ArrayList<>() {{
+                add(2);
+                add(3);
+                add(4);
+            }});
+        }};
+
+        return map;
+    }
+
+    @GetMapping("/home/returnCar")
+    @ResponseBody
+    public Car showReturnCar() {
+        Car car = new Car(1, 100, "카니발", new ArrayList<>() {{
+            add(2);
+            add(3);
+            add(4);
+        }});
+
+        return car;
+    }
+
+    @GetMapping("/home/returnCarV2")
+    @ResponseBody
+    public CarV2 showReturnCarV2() {
+        CarV2 car = new CarV2(1, 100, "카니발", new ArrayList<>() {{
+            add(2);
+            add(3);
+            add(4);
+        }});
+
+        car.setName(car.getName() + "V2");
+
+        return car;
+    }
+
+    @GetMapping("/home/returnCarMapList")
+    @ResponseBody
+    public List<Map<String, Object>> showReturnCarMapList() {
+        Map<String, Object> carMap1 = new LinkedHashMap<>() {{
+            put("id", 1);
+            put("speed", 100);
+            put("name", "카니발");
+            put("relatedIds", new ArrayList<>() {{
+                add(2);
+                add(3);
+                add(4);
+            }});
+        }};
+
+        Map<String, Object> carMap2 = new LinkedHashMap<>() {{
+            put("id", 2);
+            put("speed", 200);
+            put("name", "포르쉐");
+            put("relatedIds", new ArrayList<>() {{
+                add(5);
+                add(6);
+                add(7);
+            }});
+        }};
+
+        List<Map<String, Object>> list = new ArrayList<>();
+
+        list.add(carMap1);
+        list.add(carMap2);
+
+        return list;
+    }
+
+    @GetMapping("/home/returnCarList")
+    @ResponseBody
+    public List<CarV2> showReturnCarList() {
+        CarV2 car1 = new CarV2(1, 100, "카니발", new ArrayList<>() {{
+            add(2);
+            add(3);
+            add(4);
+        }});
+
+        CarV2 car2 = new CarV2(2, 200, "포르쉐", new ArrayList<>() {{
+            add(5);
+            add(6);
+            add(7);
+        }});
+
+        List<CarV2> list = new ArrayList<>();
+
+        list.add(car1);
+        list.add(car2);
+
+        return list;
+    }
+}
+
+@AllArgsConstructor // 안의 멤버변수를 모두 포함한 생성자를 자동으로 생성(롬복)
+@Getter // 개별적으로 포함 시키고 싶으면 변수에 하나씩 따로 붙이면 된다.
+class Person {
+    private int id;
+    @Getter
+    private String name;
+    private int age;
+}
+
+class Car {
+    private final int id;
+    private final int speed;
+    private final String name;
+    private final List<Integer> relatedIds;
+
+    public Car(int id, int speed, String name, List<Integer> relatedIds) {
+        this.id = id;
+        this.speed = speed;
+        this.name = name;
+        this.relatedIds = relatedIds;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Integer> getRelatedIds() {
+        return relatedIds;
+    }
+}
+
+@AllArgsConstructor
+//@Getter
+class CarV2 {
+    private final int id;
+    @Getter
+    private final int speed;
+    @Setter
+    @Getter
+    private String name;
+    private final List<Integer> relatedIds;
 }
