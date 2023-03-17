af2f20396d46a9581b5480a3593be7d6ede80da0
youngjae459 <youngjae459@gmail.com>
2023-03-17 15:59:20 +0900

    엔티티 객체 등록시 등록 및 수정날짜 자동갱신

--- src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -1,9 +1,14 @@
 package com.ll.basic1.boundedContext.article.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.LocalDateTime;
 
@@ -14,11 +19,14 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
     @Id // id를 주 키로 설정한다는 어노테이션
     @GeneratedValue(strategy = IDENTITY) // auto_increment 옵션
     private long id;
+    @CreatedDate
     private LocalDateTime createDate; // 데이터 생성 날짜
+    @LastModifiedDate
     private LocalDateTime modifyDate; // 데이터 수정 날짜
     private String title;
     private String body;
